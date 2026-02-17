d16b180f451f4bbf82134a5e1cb6443c8fddffc4
alessiop0512 <alessiopagliaricci@icloud.com>
2026-02-17 03:44:21 +0100

    collegamento database

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from app.database import init_db
 
 app = FastAPI()
 
+@app.on_event("startup")
+def startup():
+    init_db()
+
 @app.get("/")
 def root():
-    return {"status": "Gestionale Allbrand attivo"}
+    return {"status": "Gestionale Allbrand attivo con Database"}
